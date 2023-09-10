fbae8408fd13e0ec9dd5f35d47338381f880ed86
ciaran <ciaranregan123@gmai.com>
2023-09-10 10:25:40 +0900

    initial commit

---
+++ myaddon/runanki.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+try:
+    import bazelfixes
+
+    bazelfixes.fix_pywin32_in_bazel()
+    bazelfixes.fix_extraneous_path_in_bazel()
+except ImportError:
+    pass
+
+import aqt
+
+if not os.environ.get("ANKI_IMPORT_ONLY"):
+    aqt.run()
